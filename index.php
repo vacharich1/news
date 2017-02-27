f6ea1a7a822ce5b563249cf88dab30fbc458329d
jay <jr30149_@hotmail.com>
2017-02-27 20:12:34 +0700

    sdfsdf

--- index.php
+++ index.php
@@ -26,9 +26,104 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		//1: jayroom Ub5f45b12f0f8f8a3a08e5b52ebbcc96b
 		$text = $event['message']['text'];
-		if($event['source']['userId']=='Ub5f45b12f0f8f8a3a08e5b52ebbcc96b' || $event['source']['groupId'] =='C26d889d89b336a786c06358c1e2df27c')
+		//1: jayroom Ub5f45b12f0f8f8a3a08e5b52ebbcc96b
+		if($event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b')
 		{
-			
+			// Reply only when message sent is in 'text' format
+				if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+				{
+						// Get text sent
+						$text = $event['message']['text'];
+						
+						
+						$arr1 = str_split($text);
+						if($arr1[0] == "@")
+						{
+							$textcut = explode(" ", $text);
+							$result = count($textcut);
+							if($result <= 1)
+							{
+								$hoonname = substr($text, 1); // cut@
+								$room=$event['source']['groupId'];
+								if(preg_match("/^[a-zA-Z]+$/", $hoonname) == 1) 
+								{
+									$sql = "INSERT INTO hoon_check (id, hoonname, room)
+											VALUES ('', '$hoonname', '$room')";
+											
+									if (mysqli_query($link, $sql)) {
+											echo "New record created successfully";
+									} 
+									else {
+											echo "Error: " . $sql . "<br>" . mysqli_error($link);
+									}
+									sleep(0.3);
+									$check ="check1";
+									#echo "work code";
+									$sql = "INSERT INTO `check_capture`(`id`, `check1`) VALUES ('','$check')";
+									if (mysqli_query($link, $sql)) {
+											echo "New record created successfully";
+									} 
+									else {
+											echo "Error: " . $sql . "<br>" . mysqli_error($link);
+									}
+									
+									sleep(10);
+									$messages556 = ['type' => 'image',
+												   'originalContentUrl' => 'https://newsistatus.com/test.png',
+												   'previewImageUrl' => 'https://newsistatus.com/test.png'
+																				 
+									];
+									
+									$replyToken = $event['replyToken'];
+									
+									$url = 'https://api.line.me/v2/bot/message/reply';
+									$data = [
+										'replyToken' => $replyToken,
+										'messages' => [$messages556]
+									];
+									$post = json_encode($data);
+									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+						
+									$ch = curl_init($url);
+									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+									$result = curl_exec($ch);
+									curl_close($ch);	
+								}					
+							}
+						}
+						else
+						{
+								if($text == '##addroombyjay')
+								{
+										$replyToken = $event['replyToken'];
+										$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
+										// Make a POST Request to Messaging API to reply to sender
+										$url = 'https://api.line.me/v2/bot/message/reply';
+										$data = [
+													'replyToken' => $replyToken,
+													'messages' => [$messages55]
+												];
+										$post = json_encode($data);
+										$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+												
+										$ch = curl_init($url);
+										curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+										curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+										curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+										curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+										curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+										$result = curl_exec($ch);
+										curl_close($ch);
+								}	
+							
+							
+						}
+					
+				}
 		}
 		else
 		{
