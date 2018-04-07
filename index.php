08be134f77ad2ffe6110f841984507830dbc3f32
jay <jr30149_@hotmail.com>
2018-04-08 00:15:29 +0700

    sdfsdfsdf

--- index.php
+++ index.php
@@ -104,10 +104,60 @@ if (!is_null($events['events'])) {
 						}
 						
 						$arr1 = str_split($text);
-						if($arr1[0] == "Q" || $arr1[0] == "#" || $arr1[0] == "c" || $arr1[0] == "C")
+						if($arr1[0] == "D" || $arr1[0] == "#" || $arr1[0] == "c" || $arr1[0] == "C")
 						{
 							$textcut = explode(" ", $text);
 							$result = count($textcut);
+							
+							if($arr1[0] == "D" and $result <= 1)
+							{
+								$hoonname = substr($text, 1); // cut@
+								if($event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b')
+								{
+									$room=$event['source']['userId'];
+								}
+								else
+								{
+									$room=$event['source']['groupId'];
+								}
+								if(preg_match("/^[a-zA-Z&0-9-_]+$*/", $hoonname) == 1) 
+								{
+
+
+										if($event['source']['groupId'] == 'Cefe1b847f360c13579996d5611a24246')
+										{
+											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'fibo', 'old', '1')";
+										}
+										else if($event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b')
+										{
+											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'fibo', 'old', '1')";
+										}
+										else
+										{
+											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'fibo', 'old', '2')";
+										}
+										//$sql = "INSERT INTO hoon_check (id, hoonname, room)
+										//		VALUES ('', '$hoonname', '$room')";
+												
+										if (mysqli_query($link, $sql)) {
+												echo "New record created successfully";
+										} 
+										else {
+												echo "Error: " . $sql . "<br>" . mysqli_error($link);
+										}
+										sleep(0.3);
+										$check ="check1";
+										#echo "work code";
+										$sql = "INSERT INTO `check_capture`(`id`, `check1`) VALUES ('','$check')";
+										if (mysqli_query($link, $sql)) {
+												echo "New record created successfully";
+										} 
+										else {
+												echo "Error: " . $sql . "<br>" . mysqli_error($link);
+										}
+									
+								}					
+							}
 							
 							//send graph
 							if($textcut[0]=="C" and $result <=3 and $result >1)
@@ -188,82 +238,6 @@ if (!is_null($events['events'])) {
 										else
 										{
 											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'aaa', 'old', '2')";
-										}
-										//$sql = "INSERT INTO hoon_check (id, hoonname, room)
-										//		VALUES ('', '$hoonname', '$room')";
-												
-										if (mysqli_query($link, $sql)) {
-												echo "New record created successfully";
-										} 
-										else {
-												echo "Error: " . $sql . "<br>" . mysqli_error($link);
-										}
-										sleep(0.3);
-										$check ="check1";
-										#echo "work code";
-										$sql = "INSERT INTO `check_capture`(`id`, `check1`) VALUES ('','$check')";
-										if (mysqli_query($link, $sql)) {
-												echo "New record created successfully";
-										} 
-										else {
-												echo "Error: " . $sql . "<br>" . mysqli_error($link);
-										}
-									}
-									
-									#sleep(10);
-									#$messages556 = ['type' => 'image',
-									#			   'originalContentUrl' => 'https://newsistatus.com/test.png',
-									#			   'previewImageUrl' => 'https://newsistatus.com/test.png'
-																				 
-									#];
-									
-									#$replyToken = $event['replyToken'];
-									
-									#$url = 'https://api.line.me/v2/bot/message/reply';
-									#$data = [
-									#	'replyToken' => $replyToken,
-									#	'messages' => [$messages556]
-									#];
-									#$post = json_encode($data);
-									#$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-						#
-									#$ch = curl_init($url);
-									#curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-									#curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-									#curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-									#curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-									#curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-									#$result = curl_exec($ch);
-									#curl_close($ch);	
-								}					
-							}
-							if($arr1[0] == "Q" and $result <= 1)
-							{
-								$hoonname = substr($text, 1); // cut@
-								if($event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b')
-								{
-									$room=$event['source']['userId'];
-								}
-								else
-								{
-									$room=$event['source']['groupId'];
-								}
-								if(preg_match("/^[a-zA-Z&0-9-_]+$*/", $hoonname) == 1) 
-								{
-									//if($hoonname!="S50H17" && $hoonname!="S50M17" && $hoonname!="S50U17")
-									{
-
-										if($event['source']['groupId'] == 'Cefe1b847f360c13579996d5611a24246')
-										{
-											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'fibo', 'old', '1')";
-										}
-										else if($event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b')
-										{
-											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'fibo', 'old', '1')";
-										}
-										else
-										{
-											$sql = "INSERT INTO hoon_check (id, hoonname, room, timeframe, type, send) VALUES ('', '$hoonname', '$room' , 'fibo', 'old', '2')";
 										}
 										//$sql = "INSERT INTO hoon_check (id, hoonname, room)
 										//		VALUES ('', '$hoonname', '$room')";
