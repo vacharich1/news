b279fe86c3fe0de5703340afa0b67ba825b1bf72
vacharich1 <jr30149_@hotmail.com>
2019-06-13 11:12:42 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,28 @@
 ﻿<?php
-$hubVerifyToken = "linebot";
-$accessToken = "EAAGThEMKbNgBAMWK53A6jbtRCE9AYOuzjIMv6s0AiQ3AzSwdb45ZBbASp4Y27vGnFZB93bgWfvZBdKZC9AHCP0RZA2hHRzZCf1MZClfun8bqK6rfs9nvfVMQR4WXakUEz3GW59rO8XvLIyZCH4Vi6fXnQsT5Adp4OZCXqZCzJIrK1kXQZDZD";
-
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-$mode_token = $_REQUEST['hub_mode'];
-if ($mode_token === 'subscribe' && $verify_token === $hubVerifyToken) 
-{
-	echo $challenge;
-}
-
+// parameters
+$hubVerifyToken = 'TOKEN123456abcd';
+$accessToken = "EAAGThEMKbNgBAI8nfxPJIeiebppuUbGQW1yaN3Q7e5dHcPOL55ZBj9n4MzCH7xb5vobhCA6G5ENtRpoXeGaSfmzGBOQDHC2LWG0FzmKwrAWyTJAAdD55SqSYXnHhWgQx7osZAhju3hfpr48uGDFCKTI7bNQis1tMLzOfmkfQZDZD";
 // check token at setup
-//if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-//  echo $_REQUEST['hub_challenge'];
-//  exit;
-//}
-
+if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+  echo $_REQUEST['hub_challenge'];
+  exit;
+}
 // handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
-
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-$response = null;
-
-//set Message
+$answer = "I don't understand. Ask me 'hi'.";
 if($messageText == "hi") {
     $answer = "Hello";
 }
-
-//send message to facebook bot
 $response = [
     'recipient' => [ 'id' => $senderId ],
     'message' => [ 'text' => $answer ]
 ];
-
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
-$result = curl_exec($ch);
-}
+curl_exec($ch);
 curl_close($ch);
+//based on http://stackoverflow.com/questions/36803518
