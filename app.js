3645f8d8114f8157c40714504fc15430891a7556
vacharich1 <jr30149_@hotmail.com>
2019-06-14 08:28:57 -0700

    sdfsdfsdf

--- app.js
+++ app.js
@@ -234,6 +234,7 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
   var quickReply = message.quick_reply;
+
   if (isEcho) {
     // Just logging message echoes to console
     console.log("Received echo for message %s and app %d with metadata %s", 
@@ -308,7 +309,6 @@ function receivedMessage(event) {
 
       default:
         sendTextMessage(senderID, messageText);
-
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
